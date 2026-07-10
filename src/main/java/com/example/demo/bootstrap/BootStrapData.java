002248876f807e272876058560c3092194e66129
weslysmith863 <weslysmith863@gmail.com>
2026-07-10 16:17:31 -0500

    B: Sprint 2 - Rebrand bootstrap data to Heritage Carriage Works, fix form field labels

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -44,61 +44,61 @@ public class BootStrapData implements CommandLineRunner {
             System.out.println(part.getName()+" "+part.getCompanyName());
         }
 
-        // Part E: Add 5 Parts and 5 Products if the database is empty
+        // Seed 5 Parts and 5 Products if the database is empty
         if (partRepository.count() == 0 && productRepository.count() == 0) {
 
-            // --- 5 ROCKET PARTS ---
+            // --- 5 CARRIAGE PARTS ---
             OutsourcedPart part1 = new OutsourcedPart();
-            part1.setName("Vacuum Merlin Engine");
-            part1.setPrice(250000.00);
-            part1.setInv(5);
-            part1.setMinInv(1);
-            part1.setMaxInv(50);
-            part1.setCompanyName("SpaceX Systems");
+            part1.setName("Spoke Wheel Assembly");
+            part1.setPrice(425.00);
+            part1.setInv(8);
+            part1.setMinInv(2);
+            part1.setMaxInv(40);
+            part1.setCompanyName("Bluegrass Wheel Co.");
             partRepository.save(part1);
 
             OutsourcedPart part2 = new OutsourcedPart();
-            part2.setName("Carbon Composite Fairing");
-            part2.setPrice(45000.00);
-            part2.setInv(12);
-            part2.setMinInv(1);
-            part2.setMaxInv(50);
-            part2.setCompanyName("Plastex Aerospace");
+            part2.setName("Forged Steel Axle");
+            part2.setPrice(310.00);
+            part2.setInv(10);
+            part2.setMinInv(2);
+            part2.setMaxInv(40);
+            part2.setCompanyName("Cumberland Ironworks");
             partRepository.save(part2);
 
             OutsourcedPart part3 = new OutsourcedPart();
-            part3.setName("Flight Computer Grid Fin");
-            part3.setPrice(18500.00);
-            part3.setInv(8);
+            part3.setName("Leather Bench Seat");
+            part3.setPrice(780.00);
+            part3.setInv(6);
             part3.setMinInv(1);
-            part3.setMaxInv(50);
-            part3.setCompanyName("Guidance Avionics");
+            part3.setMaxInv(20);
+            part3.setCompanyName("Lexington Saddlery");
             partRepository.save(part3);
 
             OutsourcedPart part4 = new OutsourcedPart();
-            part4.setName("Liquid Oxygen (LOX) Tank");
-            part4.setPrice(85000.00);
+            part4.setName("Draft Horse Harness Set");
+            part4.setPrice(1250.00);
             part4.setInv(4);
             part4.setMinInv(1);
-            part4.setMaxInv(50);
-            part4.setCompanyName("CryoFuel Logistics");
+            part4.setMaxInv(15);
+            part4.setCompanyName("Lexington Saddlery");
             partRepository.save(part4);
 
             OutsourcedPart part5 = new OutsourcedPart();
-            part5.setName("Cold Gas Thruster Assembly");
-            part5.setPrice(12500.00);
-            part5.setInv(15);
-            part5.setMinInv(1);
+            part5.setName("Hickory Carriage Body Panel");
+            part5.setPrice(560.00);
+            part5.setInv(12);
+            part5.setMinInv(2);
             part5.setMaxInv(50);
-            part5.setCompanyName("OrbitTech Propulsion");
+            part5.setCompanyName("Appalachian Timber Mills");
             partRepository.save(part5);
 
-            // --- 5 ROCKET PRODUCTS ---
-            Product prod1 = new Product("Falcon Heavy Lift Kit", 1200000.00, 3);
-            Product prod2 = new Product("Orbital Delivery Stage", 650000.00, 5);
-            Product prod3 = new Product("LEO Satellite Base", 320000.00, 7);
-            Product prod4 = new Product("Suborbital Test Vehicle", 150000.00, 2);
-            Product prod5 = new Product("Deep Space Cargo Unit", 980000.00, 4);
+            // --- 5 CARRIAGE PRODUCTS ---
+            Product prod1 = new Product("Classic Vis-a-Vis Carriage", 18500.00, 2);
+            Product prod2 = new Product("Surrey with Fringe Top", 12800.00, 3);
+            Product prod3 = new Product("Meadowbrook Cart", 4200.00, 5);
+            Product prod4 = new Product("Formal Dress Landau", 22000.00, 1);
+            Product prod5 = new Product("Exercise Breaking Cart", 3100.00, 4);
 
             productRepository.save(prod1);
             productRepository.save(prod2);
